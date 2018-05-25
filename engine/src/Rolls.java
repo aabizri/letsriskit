5188c5cbbf92d2a4f3b66e7914688f15ef9cc460
Alexandre A. Bizri <alexandre@bizri.fr>
2018-05-25 11:49:31 +0200

    Simplify roll calculation in Rolls.
    Factorize rolling with new static method Rolls.rollDice.

--- engine/src/Rolls.java
+++ engine/src/Rolls.java
@@ -10,12 +10,8 @@ public class Rolls {
         this.attackers = new HashMap<>(attackingParty.size());
         this.defenders = new HashMap<>(defendingParty.size());
 
-        attackingParty.forEach(u -> attackers.put(u,
-                u.getType().getMinPower() + rand.nextInt(u.getType().getMaxPower() + 1)
-        ));
-        defendingParty.forEach(u -> defenders.put(u,
-                u.getType().getMinPower() + rand.nextInt(u.getType().getMaxPower() + 1)
-        ));
+        attackingParty.forEach(u -> attackers.put(u, Rolls.rollDice(rand,u.getType())));
+        defendingParty.forEach(u -> defenders.put(u, Rolls.rollDice(rand,u.getType())));
     }
 
     public Map<Unit, Integer> getAttackerRolls() {
@@ -24,5 +20,9 @@ public class Rolls {
 
     public Map<Unit, Integer> getDefenderRolls() {
         return new HashMap<>(defenders);
+    }
+
+    private static int rollDice(Random rand, UnitType ut) {
+        return ut.getMinPower() + rand.nextInt(ut.getMaxPower() + 1);
     }
 }
