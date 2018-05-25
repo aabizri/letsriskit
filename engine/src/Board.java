3025fb992fd771183ca08653107aa0b0930b05e1
Alexandre A. Bizri <alexandre@bizri.fr>
2018-05-25 12:09:24 +0200

    Add the Board to centralize territory, region and unit management.

---
+++ engine/src/Board.java
@@ -0,0 +1,24 @@
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class Board {
+    private Collection<Region> regions;
+    private UnitRegistry units;
+
+    public void register(Unit u) {
+        units.register(u);
+    }
+
+    public Collection<Region> getRegions() {
+        return regions;
+    }
+
+
+    public Collection<Territory> getTerritories() {
+        return regions.stream().flatMap(r -> r.getTerritories().stream()).collect(Collectors.toList());
+    }
+
+    public UnitRegistry getUnits() {
+        return units;
+    }
+}
