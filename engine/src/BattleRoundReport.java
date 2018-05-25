38416803f3f8c5f8a87f4ea333168badf07aae97
Alexandre A. Bizri <alexandre@bizri.fr>
2018-05-25 11:48:17 +0200

    Add victory calculation to BattleRoundReport.

--- engine/src/BattleRoundReport.java
+++ engine/src/BattleRoundReport.java
@@ -1,23 +1,38 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class BattleRoundReport {
     private Rolls rolls;
-    private Map<Unit, Boolean> attackers;
+    private Map<Unit, Boolean> attackers; // FALSE: DEAD, TRUE: ALIVE
     private Map<Unit, Boolean> defenders;
-    private boolean attackerVictory;
+    private boolean attackerVictorious;
 
     BattleRoundReport(Rolls r, Map<Unit, Boolean> attackers, Map<Unit, Boolean> defenders) {
         this.rolls = r;
+
         this.attackers = attackers;
         this.defenders = defenders;
+
+        this.attackerVictorious = evaluateAttackerVictory(defenders);
     }
 
     /**
-     * Commit the BattleRound to history, applying the deaths on the defeated units
+     * Checks if the attacker was victorious.
+     */
+    private static boolean evaluateAttackerVictory(Map<Unit, Boolean> defenders) {
+        return defenders.values().stream().noneMatch(b -> b);
+    }
+
+    /**
+     * Commit the BattleRound to history, applying the deaths on the defeated units.
      */
     public void commit() {
-        Stream.concat(attackers.entrySet().stream(),defenders.entrySet().stream()).filter(entry -> !entry.getValue()).forEach(entry -> entry.getKey().kill());
+        Stream.concat(
+                attackers.entrySet().stream(),
+                defenders.entrySet().stream())
+
+                .filter(entry -> !entry.getValue()).forEach(entry -> entry.getKey().kill());
     }
 
     public Rolls getRolls() {
@@ -25,14 +40,14 @@ public class BattleRoundReport {
     }
 
     public Map<Unit, Boolean> getAttackers() {
-        return attackers;
+        return new HashMap<>(attackers);
     }
 
     public Map<Unit, Boolean> getDefenders() {
-        return defenders;
+        return new HashMap<>(defenders);
     }
 
     public boolean isAttackerVictorious() {
-        return attackerVictory;
+        return attackerVictorious;
     }
 }
