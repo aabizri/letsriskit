2b0287fa5c0a8a66c1dc90e286ed7f55a53e77b0
Alexandre A. Bizri <alexandre@bizri.fr>
2018-05-26 00:44:55 +0200

    Add documentation, annotations & assertions to Mission & SubMission.
    Add method getObjective to Mission.

--- engine/src/Mission.java
+++ engine/src/Mission.java
@@ -1,19 +1,37 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Immutable, so thread-safe as well
+ */
 public class Mission {
-    private String objective;
-    private Collection<SubMission> subMissions;
+    @NotNull private final String objective;
+    @NotNull private final Collection<@NotNull SubMission> subMissions;
 
-    public Mission(String objective, Collection<SubMission> subMissions) {
+    public Mission(@NotNull String objective, @NotNull Collection<@NotNull SubMission> subMissions) {
+        assert(objective != null); assert(subMissions != null);
+        assert(subMissions.size() > 0);
+        assert(subMissions.stream().allMatch(sm -> sm != null));
+
         this.objective = objective;
         this.subMissions = subMissions;
     }
 
-    public Collection<SubMission> getSubMissions() {
-        return subMissions;
+    @NotNull
+    public String getObjective() {
+        return this.objective;
     }
 
-    public boolean isAchieved(Player player, Board board) {
+    @NotNull
+    public Collection<SubMission> getSubMissions() {
+        return new ArrayList<>(subMissions);
+    }
+
+    public boolean isAchieved(@NotNull Player player, @NotNull Board board) {
+        assert(player != null); assert(board != null);
+
         return subMissions.stream().allMatch(sm -> sm.isAchieved(player,board));
     }
 }
--- engine/src/SubMission.java
+++ engine/src/SubMission.java
@@ -1,16 +1,24 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.BiFunction;
 
+/**
+ * Immutable, so thread-safe as well
+ */
 public class SubMission {
-    private String objective;
-    private int target;
-    private BiFunction<Player,Board,Integer> checker;
+    @NotNull private final String objective;
+    private final int target;
+    @NotNull private final BiFunction<Player,Board,Integer> checker;
 
-    public SubMission(String objective, int target, BiFunction<Player,Board,Integer> checker) {
+    public SubMission(@NotNull String objective, int target, @NotNull BiFunction<Player,Board,Integer> checker) {
+        assert(objective != null); assert(checker != null);
+
         this.objective = objective;
         this.target = target;
         this.checker = checker;
     }
 
+    @NotNull
     public String getObjective() {
         return objective;
     }
@@ -19,11 +27,15 @@ public class SubMission {
         return this.target;
     }
 
-    public int getStatus(Player player, Board board) {
+    public int getStatus(@NotNull Player player, @NotNull Board board) {
+        assert(player != null); assert(board != null);
+
         return this.checker.apply(player,board);
     }
 
-    public boolean isAchieved(Player player, Board board) {
+    public boolean isAchieved(@NotNull Player player, @NotNull Board board) {
+        assert(player != null); assert(board != null);
+
         return this.getStatus(player,board) == this.getTarget();
     }
 
