a05ea7dae7061ec6f5f67348049382d416659805
Dinesh Anthonipillai <dinesh77240@gmail.com>
2018-05-31 15:50:26 +0200

    Supprimes quelques lignes inutiles.

--- interface/src/risk.java
+++ interface/src/risk.java
@@ -9,57 +9,40 @@ import javax.swing.*;
 
 public class risk extends JFrame {
 
-
-    //Le menu
     private JFrame f = new JFrame();
     private JPanel menu = new JPanel();
-    private JPanel map = new JPanel();
-    public risk(){
 
+    public risk(){
 
         initUI();
     }
 
-
-
-
     private void initUI() {
 
-        f.getContentPane().add(menu);
         menu.add(new Board());
         menu.setVisible(true);
-
-        f.getContentPane().add(map);
         add(menu);
         pack();
-
 
         setTitle("RISK");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setSize(1280, 750);
         setResizable(false);
+
         Map m = new Map();
         menu.add(m);
         Collection<Territory>territories = m.initWorldTerritories();
         territories.forEach(t -> t.addToPanel(menu));
         menu.setLayout(null);
+
        }
-
-
-
-
 
     public static void main(String[] args) {
 
         EventQueue.invokeLater(() -> {
             risk game = new risk();
             game.setVisible(true);
-        }
-        );
-
-
+        });
     }
-
-
 }
